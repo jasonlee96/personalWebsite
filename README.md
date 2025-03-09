1546f0d41d7e60a5ab55be645af195c98be472f0
Jason Lee <jasonlee@alpharedservices.com>
2025-03-09 22:22:04 +0800

    set up gh page

--- README.md
+++
@@ -1,4 +0,0 @@
-# personalWebsite
-Personal website that built using VueJS
-
-Website: https://jasonlee96.github.io/personalWebsite/
--- src/components/index.js
+++
@@ -1,8 +0,0 @@
-import HomePage from './HomePage.vue';
-import Bio from './Bio.vue';
-import Education from './Education.vue';
-import Experience from './Experience.vue';
-import Skill from './Skill.vue';
-import ProjectItem from './ProjectItem.vue';
-
-export { HomePage, Bio, Education, Experience, Skill, ProjectItem };
