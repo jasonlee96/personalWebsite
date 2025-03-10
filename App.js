a481184ed19bf0df44672625d3ed726ee9f5e834
PC <jasonleezhiyong@gmail.com>
2025-03-10 19:07:48 +0800

    Updates

--- App.js
+++ App.js
@@ -12,6 +12,7 @@ const App = () => {
       <ParallaxDot />
       <h1 className="text-5xl md:text-6xl font-extrabold mb-32 md:mb-48 text-[#5a3e1b] drop-shadow-lg text-center leading-tight">Hello! <br /> My Name is Jason Lee Zhi Yong.</h1>
       <div className="w-full max-w-5xl">
+        <></>
         <Summary />
         <Experience />
         <Skill />
